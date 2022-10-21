34b244bd9083d23d951880dbe15f9dbe9b998f4f
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2022-10-21 20:52:39 +0530

    Time: 43 ms (85.12%), Space: 7.2 MB (17.48%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,38 @@
+class Foo {
+     std::mutex m;
+     std:: condition_variable cv;
+    int turn =0 ;
+public:
+    Foo() {
+       turn =0;
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        turn =1;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        std::unique_lock<std::mutex> lock(m);
+        while(turn!=1) 
+            cv.wait(lock);
+        printSecond();
+        turn =2;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        
+        // printThird() outputs "third". Do not change or remove this line.
+         std::unique_lock<std::mutex> lock(m);
+        while(turn!=2){
+            cv.wait(lock);
+        }
+        printThird();
+    }
+};
