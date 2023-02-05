09f2485102a7745067adf493549d7f822be0e800
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-02-05 12:12:04 +0530

    Time: 67 ms (18.54%), Space: 12.8 MB (13.53%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -6,22 +6,25 @@ public:
         vector<int>ans;
         if(np>ns)return {};
         
-        vector<int> arr_p(26,0);
-        vector<int> arr_s(26,0);
+        // vector<int> arr_p(26,0);
+        // vector<int> arr_s(26,0);
+        
+        unordered_map<char,int>arr_p,arr_s;
         
         
         
         for(int i=0;i<np;i++){
             
-            arr_p[p[i]-'a']++;
-            arr_s[s[i]-'a']++;
+            arr_p[p[i]]++;
+            arr_s[s[i]]++;
         }
         
         if(arr_p==arr_s)ans.push_back(0);
         
         for(int i=np;i<ns;i++){
-            arr_s[s[i-np]-'a']--;
-            arr_s[s[i]-'a']++;
+            arr_s[s[i-np]]--;
+            if(arr_s[s[i-np]]==0) arr_s.erase(s[i-np]);
+            arr_s[s[i]]++;
             if(arr_p==arr_s)ans.push_back(i-np+1);
             
         }
