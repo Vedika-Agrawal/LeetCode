454ba980c40017d4774ca91f01f3de619c86d93c
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-02-21 08:46:53 +0530

    Time: 35 ms (28.34%), Space: 22.4 MB (16.37%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>&v) {
+        int n = v.size();
+        int l=0, h= n-1, mid;
+        while(l<h)
+        {
+            int mid = (l+(h-l)/2);
+            if((mid%2==0 && v[mid]==v[mid+1] ) || (mid%2!=0  && v[mid]== v[mid-1]))
+            {
+                l = mid +1;
+            }
+            else
+              h = mid;
+            
+        }
+        return v[l];
+    }
+};
