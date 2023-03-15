8e8dc97a4c722d5223c5836bcb6afe99b74266f9
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-03-15 16:42:48 +0530

    Time: 346 ms (38.99%), Space: 148.9 MB (27.80%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,15 +4,12 @@ public:
         vector<int>ans;
         priority_queue<pair<int,int>>q;
         for(int i=0;i<k;i++)q.push({v[i], i});
-        ans.push_back(q.top().first);
+        ans.push_back(q.top().first); // 3 3 
         
         for(int i=k;i<v.size();i++){
-            q.push({v[i], i}); // add curr ele to the q;
+            q.push({v[i], i}); // add curr ele to the q; // -1=0 3=1 -1=2 -3=3 5=4
             
-            while(!(q.top().second > i-k)){
-                 cout<<q.top().second<<endl;
-                 q.pop();
-            }
+            while(q.top().second<=i-k) q.pop();
                
             ans.push_back(q.top().first);
             
