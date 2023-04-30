bbade0de7dd56e93d7d3879f0d6275a23b100969
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-04-30 22:47:34 +0530

    Time: 627 ms (63.77%), Space: 139 MB (82.61%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,28 +1,103 @@
+class DSU {
+    private:
+        vector<int> parent;
+        vector<int> rank;
+        int components;
+
+    public : 
+        
+        DSU(int n) {
+            parent.resize(n+1);
+            rank.resize(n+1);
+            components = n;
+            for(int i = 1; i<=n; i++) {
+                parent[i] = i;
+            }
+        }
+    
+        int find (int x) {
+            if (x == parent[x]) 
+                return x;
+
+            return parent[x] = find(parent[x]);
+        }
+
+        void Union (int x, int y) {
+            int x_parent = find(x);
+            int y_parent = find(y);
+
+            if (x_parent == y_parent) 
+                return;
+
+            if(rank[x_parent] > rank[y_parent]) {
+                parent[y_parent] = x_parent;
+            } else if(rank[x_parent] < rank[y_parent]) {
+                parent[x_parent] = y_parent;
+            } else {
+                parent[x_parent] = y_parent;
+                rank[y_parent]++;
+            }
+            components--;
+        }
+    
+        bool isSingle() {
+            return components == 1;
+        }
+};
+
 class Solution {
 public:
-    int find(vector<int> &ds, int i) {
-    return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
-}
-int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-    vector<int> ds_both(n + 1, -1);
-    int used = 0;
-    for (int type = 3; type > 0; --type) {
-        auto ds_one = ds_both;
-        auto &ds = type == 3 ? ds_both : ds_one;
-        for (auto &e : edges)
-            if (e[0] == type) {
-                int i = find(ds, e[1]), j = find(ds, e[2]);
-                if (i != j) {
-                    ++used;
-                    if (ds[j] < ds[i])
-                        swap(i, j);
-                    ds[i] += ds[j];
-                    ds[j] = i;
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        
+        auto lambda = [&](vector<int>& v1, vector<int>& v2) {
+            return v1[0] > v2[0];
+        };
+        
+        sort(begin(edges), end(edges), lambda);
+        DSU Alice(n);
+        DSU Bob(n);
+        
+        int addedEdge = 0;
+        
+        for (auto &edge: edges) { // For each edge -
+            int type = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+            
+            if(type == 3) {
+                bool add = false;
+                
+                if(Alice.find(u) != Alice.find(v)) {
+                    Alice.Union(u, v);
+                    add = true;
+                }
+                
+                if(Bob.find(u) != Bob.find(v)) {
+                    Bob.Union(u, v);
+                    add = true;
+                }
+                
+                if(add)
+                    addedEdge++;
+                
+            } else if(type == 2) {
+                if(Bob.find(u) != Bob.find(v)) {
+                    Bob.Union(u, v);
+                    addedEdge++;
+                }
+            } else {
+                if(Alice.find(u) != Alice.find(v)) {
+                    Alice.Union(u, v);
+                    addedEdge++;
                 }
             }
-        if (type != 3 && ds[find(ds, 1)] != -n)
-            return -1;
+        }
+        
+        
+        if(Alice.isSingle() && Bob.isSingle()) {
+            return edges.size() - addedEdge;
+        }
+        
+        return -1;
     }
-    return edges.size() - used;
-}
 };
