ac9fb4a9bae897b0afae6d0d440e0f7ddf65ffcc
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-25 21:59:43 +0530

    Time: 1298 ms (52.55%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -5,3 +5,13 @@ from
 WHERE E.DepartmentId = D.id 
   AND (DepartmentId,Salary) in 
   (SELECT DepartmentId,max(Salary) as max FROM Employee GROUP BY DepartmentId)
+
+# SELECT d.name as department, e.name as employee, E.Salary 
+# from
+# employee e ,
+# department d
+# where e.departmentId = d.id
+
+# select D.name as Department, E.name as Employee, E.salary as Salary from Employee as E, Department as D
+# where E.departmentId=D.id
+# and (E.departmentId,E.salary) in (select departmentId,max(salary) from Employee group by departmentId);
