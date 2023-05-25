afafa250f9b29314fe99a535c4cb8879dddf564d
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-25 14:53:47 +0530

    Time: 841 ms (22.74%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -3,7 +3,11 @@
 
 # SELECT email from (select email, count(email) as cnt from person group by email) temp where temp.cnt>
 
-select Email
-from Person
-group by Email
-having count(email) > 1
+# select Email
+# from Person
+# group by Email
+# having count(email) > 1
+
+SELECT  distinct p1.email from person p1, person p2
+where p1.id <> p2.id 
+AND p1.email=p2.email;
