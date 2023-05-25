963c05066a74f87db2e8d10340251fb31b78591b
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-25 12:31:08 +0530

    Time: 467 ms (39.52%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -6,4 +6,6 @@
 # salary < (SELECT MAX(salary) FROM Employee);
 
 
-SELECT IFNULL ((SELECT distinct salary FROM Employee order by salary desc limit 1 offset 1),NULL) as SecondHighestSalary
+# SELECT IFNULL ((SELECT distinct salary FROM Employee order by salary desc limit 1 offset 1),NULL) as SecondHighestSalary
+
+SELECT IFNULL ((SELECT distinct salary from employee order by salary desc limit 1 offset 1),NULL) as  SecondHighestSalary
