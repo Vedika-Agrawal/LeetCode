6f897dba6466a10f5708b42a143102abbba04f0f
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-25 23:25:00 +0530

    Time: 1383 ms (95.30%), Space: 0B (100.00%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,8 @@
+select d.name as department, e.name as employee, e.salary
+from 
+(select * ,dense_rank()  over (partition by departmentId order by salary desc ) as r from employee) e
+INNER JOIN 
+department as d 
+on 
+d.id = e.departmentId
+where r<=3;
