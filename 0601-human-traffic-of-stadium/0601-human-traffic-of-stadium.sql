61e8be0b00b1c8b51aced9b3b04414b543a36fca
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-26 16:18:03 +0530

    Time: 828 ms (16.31%), Space: 0B (100.00%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,9 @@
+select distinct s1.* from
+Stadium s1,Stadium s2,Stadium s3
+where s1.people>=100 and s2.people>=100 and s3.people>=100
+    and ((s1.id=s2.id-1 and s1.id = s3.id-2 ) 
+     or
+     (s1.id=s2.id+1 and s1.id = s3.id-1 ) 
+     or 
+     (s1.id=s2.id+1 and s1.id = s3.id+2 ))
+     order by id;
