f20fde93150442dd45a3539fc9e140cebb743d84
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-26 15:19:29 +0530

    Time: 927 ms (59.14%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,4 +1,3 @@
-select customer_number from orders 
-group by customer_number 
-order by count(order_number) desc
+select  customer_number from orders 
+group by customer_number order by count(order_number) desc 
 limit 1;
