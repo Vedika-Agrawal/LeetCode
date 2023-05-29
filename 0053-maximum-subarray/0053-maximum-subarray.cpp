5d0b8592ed5a61e94794209c1d63399275417281
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-29 15:24:06 +0530

    Time: 124 ms (44.39%), Space: 67.7 MB (50.55%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans =INT_MIN , curr=0 ;
+        for(int i=0;i<nums.size();i++){
+            curr += nums[i];
+            ans = max(ans, curr);
+            if(curr<0)curr =0 ;
+        }
+        return ans;
+    }
+};
