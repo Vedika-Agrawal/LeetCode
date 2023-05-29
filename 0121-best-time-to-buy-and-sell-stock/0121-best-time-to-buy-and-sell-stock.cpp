7fce4e9b3e61d29721e9bf18671e0c54c232e2c4
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-29 17:00:18 +0530

    Time: 157 ms (18.64%), Space: 93.4 MB (10.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     
-    int maxProfit(vector<int>& v) {
+   int maxProfit(vector<int>& v) {
+        if(v.size()==1){
+            return 0;
+        }
         int mini = v[0];
-        int ans =INT_MIN ;
+        int ans = 0;
         for(int i=0;i<v.size();i++){
-        
-            mini =min(mini, v[i]);
             ans = max(ans, v[i]-mini);
-            
+            mini =min(mini, v[i]);
         }
         return ans;
     }
