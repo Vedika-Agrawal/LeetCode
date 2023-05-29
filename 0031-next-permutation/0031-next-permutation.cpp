49a296a8f3e86ec5073d6ce4b860ee91cf01163d
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-29 15:18:37 +0530

    Time: 7 ms (37.40%), Space: 12.2 MB (29.00%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-         next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        int i,k;
+        
+        for( i=n-2 ;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                break;
+            }
+        }
+     
+        if(i<0){
+            reverse(nums.begin(), nums.end());
+            return ;
+        }
+        else{
+            for( k = n-1; k>i ; k--){
+                if(nums[k]>nums[i]){
+                    break;
+                }
+            }
+            
+        }
+        swap(nums[k], nums[i]);
+    	reverse(nums.begin() + i + 1, nums.end());
+        
     }
 };
+
+
+    	    
