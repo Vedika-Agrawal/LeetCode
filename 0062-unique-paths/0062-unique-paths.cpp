a42d99c7c6f29108cb37d8ee7558acb9a29502c2
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-31 10:25:45 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (27.26%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int n,m;
+    
+    int solve(vector<vector<int>>&vis, int r, int c){
+        
+        if(r>=n || c>=m )return 0;
+        
+        if(r==n-1 && c ==m-1)return 1;
+        
+        if( vis[r][c] != -1)return vis[r][c];
+        
+        int right = solve(vis,r,c+1);
+        int down = solve(vis,r+1,c);
+        vis[r][c]=0;
+        
+        
+        return vis[r][c] = right+down;
+        
+        
+    }
+    
+    
+    int uniquePaths(int r, int c) {
+        n=r,m=c;
+        
+        vector<vector<int>>vis(n,vector<int>(m,-1));
+    
+        return solve(vis,0,0);
+    }
+};
