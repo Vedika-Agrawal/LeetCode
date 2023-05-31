42e9954cc773dbe555cc3886b42f9990f1b222aa
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-05-31 23:01:20 +0530

    Time: 32 ms (34.86%), Space: 10 MB (36.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start)
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+     int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+        map<char,int> m;
+        int i=0,j=0;
+        int ans=0;
+        while(j<n){
+            m[s[j]]++;
+            if(j-i+1==m.size()){
+                ans=max(ans,(j-i+1));
+            }
+            else{
+                while((j-i+1)>m.size()){
+                    m[s[i]]--;
+                    if(m[s[i]]==0){
+                        m.erase(s[i]);
+                    }
+                    i++;
+                }
+                if(j-i+1==m.size()){
+                    ans=max(ans,(j-i+1));
+                }
+            }
+            j++;
         }
-        return maxLen;
+        return ans;
     }
-
 };
