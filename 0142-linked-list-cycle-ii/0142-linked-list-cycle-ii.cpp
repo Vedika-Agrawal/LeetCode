de54a408d0cdd92223884b741c22964cb9ca1251
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-01 12:22:12 +0530

    Time: 12 ms (42.70%), Space: 7.5 MB (83.26%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -13,7 +13,7 @@ public:
         ListNode * slow= head;
         ListNode * fast = head ;
         ListNode * entry = head;
-        while( fast->next !=NULL && fast->next->next!=NULL){
+        while( fast !=NULL && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
             if(slow==fast){
