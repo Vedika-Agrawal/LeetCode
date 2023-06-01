b80f48c7e77ea81cf38f015cdb0d50af28ca34d1
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-01 23:00:57 +0530

    Time: 12 ms (37.93%), Space: 11.2 MB (66.92%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -15,68 +15,62 @@ public:
 */
 
 class Solution {
-    
-    
 public:
-    void copyll(Node* & head){
-        if(!head) return ;
-        Node* node = head;
-        // while(curr){
-        //     Node * node = new Node(curr->val);
-        //     node->next = curr->next;
-        //     curr->next = node;
-        //     curr = node->next;
-        // }
-        while(node){
-        Node* temp=node->next;
-        node->next=new Node(node->val);
-        node->next->next=temp;
-        node=temp;
-    }
-    }
-    void copyrp(Node* & head){
+    
+    void copyll ( Node* head){
         if(!head)return ;
-        Node* node = head;
-        // while(curr){
-        //     curr->next->random = curr->random ? curr->random->next:NULL;
-        //     curr= curr->next->next;
-        // }
-        while(node){
-            if(node->random)
-            node->next->random=node->random->next;
-           node=node->next->next; // go to next old node
+        Node* curr = head;
+        while(curr){
+            Node* temp = curr->next;
+            curr->next = new Node(curr->val);
+            curr->next->next = temp;
+            curr = temp;
         }
-}
+    }
+    
+    void copyrp(Node* head){
+        if(!head)return ;
+        Node * curr = head;
+      
         
+        while(curr){
+            if(curr->random)
+            curr->next->random=curr->random->next;
+           curr=curr->next->next; 
+        }
+        
+    }
+    
+    
+    
+    
     
     Node* copyRandomList(Node* head) {
+        
+        
         copyll(head); 
         copyrp(head);
+        Node* curr = head;
+        Node* dummy = new Node(0);
+        Node* curr2 =  dummy;
         
-//         Node*ori =head;
-//         Node* clone = head->next ,* nhead = head->next;
         
-//         while(ori){
-//             ori->next=ori->next->next;
-//             clone->next = clone->next? clone->next->next: NULL;
-//             ori = ori->next;
-//             clone = clone->next;
-//         }
-        
-//         return nhead;
-        
-        Node* ans=new Node(0); // first node is a dummy node
-        Node* helper=ans;
-
-        while(head){
-            // Copy the alternate added nodes from the old linklist
-            helper->next=head->next;
-            helper=helper->next;
-
-            // Restoring the old linklist, by removing the alternative newly added nodes
-            head->next=head->next->next;
-            head=head->next; // go to next alternate node   
+        while(curr){
+            
+            
+             // curr2->next=curr2->next?curr2->next->next:NULL;
+             curr2->next = curr->next;
+             curr2 = curr2->next;
+            
+            
+           curr->next = curr->next->next;
+           
+            curr = curr->next;
+           
         }
-        return ans->next; 
+        
+        return dummy->next;
+        
+        
     }
 };
