4915f70edbd1826055300bedac2ef89e23e03c85
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-03 00:53:47 +0530

    Time: 66 ms (18.27%), Space: 41.1 MB (19.00%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    int n;
-    void solve(vector<int>& can, int target, int idx, vector<int> temp){
-        if(target==0){
-            ans.push_back(temp);
-            return ;
+   vector<vector<int>> res;
+    void solve(vector<int>& candidates,int i,vector<int> temp,int target){
+        if(i==candidates.size()){
+            return;
         }
-        if(idx==n)return ;
+         if(target==0){
+            res.push_back(temp);
+             return;
+        }
         
-        
-        solve(can,target, idx+1, temp);
-        temp.push_back(can[idx]);
-        if(target-can[idx] >=0)
-        solve(can,target - can[idx], idx, temp);
-        
+        solve(candidates,i+1,temp,target);
+        if(target-candidates[i]>=0){
+            temp.push_back(candidates[i]);
+            target-=candidates[i];
+            solve(candidates,i,temp,target);
+        }
+       
     }
-    
-    vector<vector<int>> combinationSum(vector<int>& can, int tar) {
-        vector<int>temp;
-        n = can.size();
-        solve(can, tar, 0, temp );
-        return ans;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n=candidates.size();
+        vector<int> temp;
+        solve(candidates,0,temp,target);
+        return res;
     }
 };
