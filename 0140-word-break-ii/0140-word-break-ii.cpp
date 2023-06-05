16657e2aa8bb6d8842025f22bed0f98d9356b0a3
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-06 00:57:07 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (76.14%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
-    void solve(string s, vector<string>& res, unordered_set<string>& st, vector<string>&temp){
-        if(s.length() == 0){
-            string str = "";
-            for(auto it:temp){
-                str += it + " ";
-            }
-            str.pop_back();
-            res.push_back(str);
-            return;
+    
+     vector<string>ans, temp_res;
+    
+    void solve(string &s,unordered_set<string> &mp){
+    if(s.size()==0){
+        string k = "";
+        for(auto it: temp_res){
+            k+= it;
+            k+=' ';
+    
         }
-        for(int i=0;i<s.length(); i++){
-            if(st.count(s.substr(0, i+1))){
-                temp.push_back(s.substr(0, i+1));
-                solve(s.substr(i+1), res, st, temp);
-                temp.pop_back();
-            }
+        k.pop_back();
+        ans.push_back(k);
+        return ;
+    }
+    
+    for(int i=0;i<s.size();i++){
+        string temp = s.substr(0,i+1);
+        if(mp.find(temp)!=mp.end()){
+            
+          temp_res .push_back(temp);
+            
+            string rest = s.substr(i+1);
+            solve(rest,mp);
+            temp_res.pop_back();
+            
         }
     }
+    
+}
+    
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        vector<string>res, temp;
+       
         unordered_set<string>st(wordDict.begin(), wordDict.end());
         
-        solve(s, res, st, temp);
-        return res;
+        solve(s, st);
+        return ans;
     }
 };
