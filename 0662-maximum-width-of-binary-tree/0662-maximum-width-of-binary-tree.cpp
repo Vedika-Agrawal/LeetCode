951652867160b28a2ee2d93227dcabe5eba26f0b
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-10 00:50:04 +0530

    Time: 8 ms (74.24%), Space: 17.3 MB (63.26%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,31 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 class Solution {
+  #define ll long long  
+    
 public:
-    typedef unsigned long long ll;
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root)   
-            return 0;
-        queue<pair<TreeNode*, ll>> que;
-        que.push({root, 0});
-        ll maxWidth = 0;
-        
-        while(!que.empty()) {
-            int n = que.size();
-            ll f = que.front().second;
-            ll l = que.back().second;
-            maxWidth = max(maxWidth, l-f+1);
-            
-            while(n--) {
-                TreeNode* curr = que.front().first;
-                ll d          = que.front().second;
-                que.pop();
-                if(curr->left) {
-                    que.push({curr->left, 2*d+1});
+        if(!root)return 0;
+        queue<pair<TreeNode* , int>>q;
+        q.push({root,0});
+        ll ans= INT_MIN;
+        while(q.size()){
+            int n = q.size();
+             
+            ll f = q.front().second;
+            ll l = q.back().second;
+             ans = max(ans, l-f+1);
+           while(n--){
+                TreeNode* node = q.front().first;
+                ll idx = q.front().second;
+                 q.pop();
+               
+
+
+                if(node->left){
+                    q.push({node->left, idx*2 +1});
                 }
-                if(curr->right) {
-                    que.push({curr->right, 2*d+2});
+                 if(node->right){
+                    q.push({node->right, idx*2 +2});
                 }
-            }
+           }
         }
-        return maxWidth;
+        return ans;
     }
 };
