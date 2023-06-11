bbe0427247ea55be260c97715a3ec70f92f08275
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-12 00:44:44 +0530

    Time: 60 ms (14.86%), Space: 25.9 MB (89.26%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -7,26 +7,26 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * };&
  */
 class Solution {
 public:
-   TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int rootIdx = 0;
-        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
-    }
-   TreeNode* build(vector<int>& pre, vector<int>& in, int& rootIdx, int st, int end){
-       if(st>end)return NULL;
-       int pivot = st;
-       while(in[pivot]!=pre[rootIdx])pivot++;
-       rootIdx++;
+   TreeNode* solve(vector<int>& pre, vector<int>& ino, int& idx, int st, int end){
+        if(st>end)return NULL;
+       int temp = st;
+       while(ino[temp]!=pre[idx])temp++;
+       idx++;
+         
+         TreeNode* newNode = new TreeNode(ino[temp]);
        
-       TreeNode* newNode = new TreeNode(in[pivot]);
-       
-      newNode->left = build(pre, in, rootIdx, st, pivot-1);
-      newNode->right = build(pre, in, rootIdx, pivot+1, end);
+      newNode->left = solve(pre, ino, idx, st, temp-1);
+      newNode->right = solve(pre, ino, idx, temp+1, end);
        return newNode;
        
-       
-   }
+    }
+    
+    TreeNode* buildTree(vector<int>& pre, vector<int>& ino) {
+        int rootIdx = 0;
+        return solve(pre, ino, rootIdx, 0, ino.size()-1);
+    }
 };
