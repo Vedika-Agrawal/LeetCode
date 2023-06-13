dbf80815a25f4a3a24408eb4526e84b8ed5e01d1
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-13 15:25:26 +0530

    Time: 31 ms (76.77%), Space: 23.3 MB (60.57%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,10 +10,28 @@
 
 class Solution {
 public:
+    TreeNode* solve(TreeNode* root,int l, int r){
+        if(root==NULL)return root;
+        if(root->val ==l || root->val==r)return root;
+        if(root->val > l && root->val < r)return root;
+        else if(root->val >l && root->val > r)
+            return solve(root->left, l,  r);
+        else
+            return solve(root->right, l, r);
+        
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root)return NULL;
-        if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left, p,q);
-        if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p,q);
-        return root;
+        if(root==NULL)return NULL;
+        // if(root->val == p->val || root->==q->val)return root;
+        int a = p->val;
+        int b = q->val;
+        // if(a>b){swap(a,b)};
+        if(a>b){
+            int c =  a;
+            a= b;
+            b=c;
+        }
+        return solve(root, a, b);
+        
     }
 };
