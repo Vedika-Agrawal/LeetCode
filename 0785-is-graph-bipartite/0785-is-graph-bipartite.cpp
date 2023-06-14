1f44dccce21cb8282e8d7b4daa3d8bca77d1560a
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-14 19:23:17 +0530

    Time: 28 ms (50.09%), Space: 13.5 MB (52.94%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    
+    bool solve(vector<vector<int>>& graph, int node, int col, vector<int>&color){
+        
+        color[node]=col;
+        for(auto it: graph[node]){
+            
+            if(color[it]==-1){
+                if(solve(graph, it,!col , color)==false)return false;
+            }
+            else if(color[it]==col){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        
+        vector<int> color(n,-1);
+    
+        for(int i=0;i<n;i++){
+            if(color[i]==-1 ){
+                if(solve(graph , i , 0, color)==false)return false;
+            }
+        }
+        return true;
+    }
+};
