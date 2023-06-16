20ee73928b86e1d14b3dce593c1aca282ebd6ca3
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-16 18:43:48 +0530

    Time: 88 ms (38.69%), Space: 24.5 MB (45.66%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    
-    int solve(string s1, string s2){
-        vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1,0));
-        
-        for(int i=s1.size()-1;i>=0;i--){
-            for(int j=s2.size()-1;j>=0;j--){
-                int ans  = 0;
-                if(s1[i]==s2[j]){
-                    ans= 1+ dp[i+1][j+1];
-                }
-                else{
-                    ans = max(dp[i+1][j], dp[i][j+1]);
-                }
-                dp[i][j] = ans;
-            }
+    int n,m;
+    vector<vector<int>>dp;
+    int solve(string &s1, string &s2, int i , int j){
+        if(i==n || j==m)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s1[i]==s2[j]){
+           return dp[i][j]= solve(s1,s2,i+1,j+1) +1;
         }
+        else {
+            int a  = solve(s1,s2,i+1,j);
+            int b  = solve(s1,s2,i,j+1);
+           return dp[i][j]=  max(a,b);
+        }
+        // return dp[n-1][m-1];
+    }
+    int longestCommonSubsequence(string t1, string t2) {
+         n = t1.size(), m = t2.size();
+        dp.resize(n,vector<int>(m,-1));
+       
+         solve(t1,t2,0,0);
         return dp[0][0];
     }
-    int longestCommonSubsequence(string s1, string s2) {
-        return solve(s1,s2);
-    }
 };
+
