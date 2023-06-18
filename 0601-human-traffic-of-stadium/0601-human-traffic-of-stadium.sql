c4165acffc770ff51d3b1baf2a4749c413203e8c
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-18 17:32:19 +0530

    Time: 911 ms (12.76%), Space: 0B (100.00%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -2,6 +2,6 @@
     where s1.people>=100 and s2.people>=100 and s3.people>=100
     and ((s1.id=s2.id-1 and s1.id = s3.id-2)
     or (s1.id = s2.id+1 and s1.id=s3.id-1)
-    or (s1.id = s3.id+1 and s1.id= s2.id+2))
+    or (s1.id = s3.id+2 and s1.id= s2.id+1))
     order by id;
     
