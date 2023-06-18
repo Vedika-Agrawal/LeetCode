b3553798e3fb5a20ac02aac4db49d87cedb7be48
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-06-18 15:08:47 +0530

    Time: 2088 ms (40.46%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,3 +1,12 @@
+# select d.name as department, e1.name as employee, e1.salary
+# from employee e1
+# join Department d on e1.DepartmentId = d.Id
+# where (
+    
+    
+# )
+
+
 select d.name as department, e.name as employee, e.salary
 from 
 (select * ,dense_rank()  over (partition by departmentId order by salary desc ) as r from employee) e
@@ -6,3 +15,10 @@ department as d
 on 
 d.id = e.departmentId
 where r<=3;
+
+
+# WHERE (
+# SELECT COUNT(DISTINCT salary)
+# FROM Employee AS E2
+# WHERE E2.departmentid = D.id AND E2.salary >= E.salary
+# ) <= 3
