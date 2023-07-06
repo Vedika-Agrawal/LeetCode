56c15da1b083214a6b1729426f880abff571af09
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-06 14:27:34 +0530

    Time: 48 ms (29.04%), Space: 28.1 MB (71.68%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-    int minSubArrayLen(int tar, vector<int>& nums) {
-        int i=0, j=0, n = nums.size();
-        int sum = 0;
-        int ans = INT_MAX;
-        while(j<n){
-            sum += nums[j];
-            while(sum>=tar){
-                ans = min(ans,j-i+1); 
-                sum-= nums[i];
-                i++;
-            }
+    bool isPossible(vector<int>&arr,int k,int mid){
+        int sum =0 ;
+        for(int i=0;i<mid;i++)sum+=arr[i];
+        int j = mid, i=0;
+        int maxi=sum;
+        while(j<arr.size()){
+            sum-=arr[i];
+            sum += arr[j];
+            maxi = max(sum,maxi);
+            
+            i++;
             j++;
         }
-        if(ans==INT_MAX)
-        ans =0;
+        return maxi>=k;
+    }
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int lo = 1, hi = nums.size();
+        int ans =  0;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(isPossible(nums, target, mid)){
+                ans = mid;
+                hi = mid-1;
+            }
+            else{
+                lo = mid+1;
+            }
+        }
         return ans;
-        
-        
     }
 };
