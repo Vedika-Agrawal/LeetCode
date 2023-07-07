c573a5bcc467e3c90bbf44bba11dc3361e27e162
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-07 11:38:30 +0530

    Time: 97 ms (11.66%), Space: 24 MB (5.31%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
-    int maxConsecutiveAnswers(string s , int k) {
-        int st = 0; int i= 0;
-        int t =0 , f=0 ;
-        int ans = 0;
-        while(i<s.size()){
-           
+    
+    bool isPossible(string s, int mid, int k){
+        int n = s.size();
+        int t= 0, f= 0;
+        for(int i=0;i<mid;i++){
             if(s[i]=='T')t++;
             else f++;
-            int mini = min(t,f);
-            while(mini>k){
-               if(s[st]=='T')t--;
-               else f--;
-               mini = min(t,f);
-                st++;
-            }
-            ans = max(ans, t+f);
-            i++;
+        }
+        int mini = min(t,f);
+        if(mini<=k)return true;
+        
+        int i= 0;
+        while(mid<n){
+            if(s[mid]=='T')t++;
+            else f++;
             
+            if(s[i]=='T')t--;
+            else f--;
+            
+            int mini = min(t,f);
+            if(mini<=k)return true;
+            i++;
+            mid++;
+            
+        }
+        return false;
+    }
+    int maxConsecutiveAnswers(string s, int k) {
+      int l = 1, h =  s.size();
+        int ans = 0;
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(isPossible(s, mid, k)){
+                ans = mid;
+                 l = mid+1;
+            }
+            else{
+                h = mid -1;
+            }
         }
         return ans;
     }
