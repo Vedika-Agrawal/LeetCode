d392fca9da8220ba5b737055a5d2ae54af482a85
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-10 14:26:53 +0530

    Time: 4 ms (87.06%), Space: 8.9 MB (48.09%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -20,6 +20,16 @@ public:
     int minimumTotal(vector<vector<int>>& grid) {
         n = grid.size();
         dp.resize(n,vector<int>(n,-1));
-        return solve(grid, 0, 0);
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = grid[n-1][j];
+        }
+        for(int i= n-2; i>=0;i--){
+            for(int j=0;j<=i; j++){
+                dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + grid[i][j];
+            }
+            
+        }
+        return dp[0][0];
+        
     }
 };
