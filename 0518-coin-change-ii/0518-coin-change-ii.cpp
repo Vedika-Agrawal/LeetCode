b7f32f0680d2dcb03287f4fe480ce4560a718826
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-12 00:20:38 +0530

    Time: 57 ms (27.48%), Space: 18.2 MB (63.49%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    int solve(int amt, vector<int>&arr, int idx, vector<vector<int>>&dp){
-        if(idx==0){
-            return amt%arr[idx]==0 ;
+    // int solve(int amt, vector<int>&arr, int idx, vector<vector<int>>&dp){
+    //     if(idx==0){
+    //         return amt%arr[idx]==0 ;
+    //     }
+    //     if(dp[idx][amt]!=-1)return dp[idx][amt];
+    //     int notTake =  solve(amt, arr, idx-1,dp);
+    //     int take = 0;
+    //     if(arr[idx]<=amt){
+    //         take = solve(amt-arr[idx], arr, idx,dp);
+    //     }
+    //     return dp[idx][amt] = take+ notTake;
+    // }
+    
+    int change(int amount, vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>>dp(n, vector<int>(amount+1,0));
+        for(int i= 0; i<=amount; i++){
+            if(i%arr[0]==0){
+                dp[0][i] = 1;
+            }
         }
-        if(dp[idx][amt]!=-1)return dp[idx][amt];
-        int notTake =  solve(amt, arr, idx-1,dp);
-        int take = 0;
-        if(arr[idx]<=amt){
-            take = solve(amt-arr[idx], arr, idx,dp);
+        
+        for(int idx=1;idx<n;idx++){
+            for(int amt = 0; amt<= amount; amt++){
+                int notTake =  dp[idx-1][amt] ;
+                int take = 0;
+                if(arr[idx]<=amt){
+                    take = dp[idx][amt-arr[idx]];
+                }
+                dp[idx][amt] = take+ notTake;
+            }
         }
-        return dp[idx][amt] = take+ notTake;
-    }
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1,-1));
-        return solve(amount, coins, n-1,dp);
+        return dp[n-1][amount];
     }
 };
