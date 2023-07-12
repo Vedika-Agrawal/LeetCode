f07d38d52c273c2ab5c97e57d17797c4d96e61a9
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-12 10:02:41 +0530

    Time: 59 ms (72.99%), Space: 10.9 MB (71.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,26 +1,24 @@
 class Solution {
-public:
-    int n,m;
-    vector<vector<int>>dp;
-    int solve(string &s1, string &s2, int i , int j){
-        if(i==n || j==m)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j]){
-           return dp[i][j]= solve(s1,s2,i+1,j+1) +1;
-        }
-        else {
-            int a  = solve(s1,s2,i+1,j);
-            int b  = solve(s1,s2,i,j+1);
-           return dp[i][j]=  max(a,b);
-        }
-        // return dp[n-1][m-1];
+public:    
+    int dp[1000 + 1][1000 + 1];
+    
+    int maxLen(string& s1, string& s2, int n, int m)
+    {
+        if (n == 0 || m == 0)
+            return 0;
+        
+        if (dp[n][m] != -1)
+            return dp[n][m];
+
+        if (s1[n - 1] == s2[m - 1])
+            return dp[n][m] = 1 + maxLen(s1, s2, n - 1, m - 1);
+        else
+            return dp[n][m] = max(0 + maxLen(s1, s2, n - 0, m - 1), 0 + maxLen(s1, s2, n - 1, m - 0));
     }
-    int longestCommonSubsequence(string t1, string t2) {
-         n = t1.size(), m = t2.size();
-        dp.resize(n,vector<int>(m,-1));
-       
-         solve(t1,t2,0,0);
-        return dp[0][0];
+        
+    int longestCommonSubsequence(string s1, string s2) 
+    {
+        memset(dp, -1, sizeof(dp));        
+        return maxLen(s1, s2, s1.length(), s2.length());
     }
 };
-
