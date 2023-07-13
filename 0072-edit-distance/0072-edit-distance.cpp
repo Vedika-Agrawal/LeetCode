f41e8f922f048ebc95c70d78083c506df41cd943
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-13 19:35:20 +0530

    Time: 14 ms (64.14%), Space: 9.1 MB (39.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int n,m;
-    int solve(string &s, string &t, int i, int j,vector<vector<int>>&dp){
-        
-        if(i==n ){
+    vector<vector<int>> dp;
+    int solve(int i,int j,string &s,string &t,vector<vector<int>>&dp){
+        if(i==n){
             return m-j;
         }
-        if(j==m)return n-i;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int same = 1e9;
+        if(j==m){
+            return n-i;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int a=1e9,b=1e9,c=1e9;
         if(s[i]==t[j]){
-            
-            return solve(s,t,i+1,j+1,dp);
+            return dp[i][j] = solve(i+1,j+1,s,t,dp);
         }
         
-        int insert = 1e9, del = 1e9, rep = 1e9;
+            a=1+solve(i+1,j+1,s,t,dp);
+            b=1+solve(i+1,j,s,t,dp);
+            c=1+solve(i,j+1,s,t,dp);
         
-        insert = 1 + solve(s,t,i,j+1,dp);
-        del = 1 + solve(s,t,i+1,j,dp);
-        rep = 1+ solve(s,t,i+1, j+1,dp);
-        return dp[i][j] = min({insert, del, rep}) ;
+        return dp[i][j]=min({a,b,c});
     }
     int minDistance(string s, string t) {
-        
-        n = s.size(), m= t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1, -1));
-        return solve(s,t,0,0,dp);
+        n=s.size();
+        m=t.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return solve(0,0,s,t,dp);
     }
 };
