67258396006b388e6aeeabc2db7ce028f891d374
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-13 19:11:52 +0530

    Time: 16 ms (56.08%), Space: 9.4 MB (21.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        if(word1==word2){
-            return 0;
+    int n,m;
+    int solve(string &s, string &t, int i, int j,vector<vector<int>>&dp){
+        
+        if(i==n ){
+            return m-j;
         }
-        int dp[n+1][m+1];
-        dp[0][0]=0;
-        for(int j=1;j<=m;j++){
-            dp[0][j]=j;
+        if(j==m)return n-i;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int same = 1e9;
+        if(s[i]==t[j]){
+            
+            return solve(s,t,i+1,j+1,dp);
         }
-        for(int i=1;i<=n;i++){
-            dp[i][0]=i;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]))+1;
-                }
-            }
-        }
-        return dp[n][m];
+        
+        int insert = 1e9, del = 1e9, rep = 1e9;
+        
+        insert = 1 + solve(s,t,i,j+1,dp);
+        del = 1 + solve(s,t,i+1,j,dp);
+        rep = 1+ solve(s,t,i+1, j+1,dp);
+        return dp[i][j] = min({insert, del, rep}) ;
+    }
+    int minDistance(string s, string t) {
+        
+        n = s.size(), m= t.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1, -1));
+        return solve(s,t,0,0,dp);
     }
 };
