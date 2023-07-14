887b42705a5decfd4fd1704a329fce2d4ea72405
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-14 11:38:10 +0530

    Time: 975 ms (5.05%), Space: 272.8 MB (5.01%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,21 +2,24 @@ typedef  vector<int> p;
 class Solution {
 public:
     int n;
-    int solve(vector<int>&v, int idx, int cnt, int buy, vector< vector<p>>&dp){
-        if(idx==n || cnt==2)return 0;
-        if(dp[idx][buy][cnt] !=-1)return dp[idx][buy][cnt];
-        int profit = 0;
-        if(buy){
-            profit  = max( -v[idx] + solve(v, idx+1, cnt, 0, dp), solve(v,idx+1,cnt,1, dp));
-        }
-        else{
-             profit  = max( v[idx] + solve(v, idx+1, cnt+1, 1, dp), solve(v,idx+1,cnt,0, dp));
-        }
-        return dp[idx][buy][cnt] = profit;
-    }
     int maxProfit(vector<int>& v) {
         n = v.size();
-        vector< vector<p>>dp(n, vector<p>(2, p(2,-1)));
-        return solve(v,0,0, 1,dp);
+        vector< vector<p>>dp(n+1, vector<p>(3, p(3,0)));
+        
+        for(int idx= n-1; idx>=0;idx--){
+            for(int buy = 0; buy<2;buy++){
+                for(int cnt =0; cnt<2;cnt++){
+                    int profit = 0;
+                    if(buy==0) profit  = max( -v[idx] + dp[idx+1][1][cnt], dp[idx+1][0][cnt]);
+                    else profit  = max( v[idx] + dp[idx+1][0][cnt+1], dp[idx+1][1][cnt]);
+                     
+                    
+                    dp[idx][buy][cnt] = profit;   
+                    
+                }
+            }
+        }
+        return dp[0][0][0];
     }
+    
 };
