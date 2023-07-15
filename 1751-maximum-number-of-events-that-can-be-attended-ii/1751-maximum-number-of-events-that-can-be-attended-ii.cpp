faf5cbd8d663b81d2223ce7debdc75af68216aec
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-15 15:24:08 +0530

    Time: 310 ms (85.15%), Space: 72.8 MB (71.72%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,40 +1,37 @@
-
 class Solution {
 public:
     int n;
-    typedef vector<int> p;
-    int solve(vector<vector<int>>&nums, int k, int idx, int prev, vector<p>&dp){
-        if(idx==n || k==0)return 0;
-        if(dp[k][prev+1]!=-1)return dp[k][prev+1];
-        int consider = 0;
-        int notConsider = solve(nums, k, idx+1, prev,dp);
-        if(prev==-1){
-            int val = nums[idx][2];
-            
-            consider = val + solve(nums, k-1 ,idx+1, idx,dp);
-        }
-        else{
-            int pst = nums[prev][0];
-            int pend = nums[prev][1];
-            int pval = nums[prev][2];
-            
-            int cst = nums[idx][0];
-            int cend = nums[idx][1];
-            int cval = nums[idx][2];
-            
-            if(pend < cst){
-                consider =  cval + solve(nums, k-1, idx+1, idx,dp);
+    vector<vector<int>>dp;
+    int search(vector<vector<int>>&nums, int val){
+        int l = 0, h =  nums.size()-1;
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(nums[mid][0] > val){
+                h = mid-1;
             }
-            
+            else{
+                l = mid+1;
+            }
         }
-        return  dp[k][prev+1]= max(consider, notConsider);
+        return l;
+    }
+    
+    int solve(vector<vector<int>>&nums, int k, int idx){
+        if(idx==n || k==0)return 0;
+        if(dp[idx][k]!=-1)return dp[idx][k];
+        int next = search(nums, nums[idx][1]);
         
+        int take = 0, nontake =0 ;
+        take = nums[idx][2] + solve(nums, k-1, next);
+        nontake =solve(nums, k, idx+1);
+        return  dp[idx][k] = max(nontake, take);
         
     }
+    
     int maxValue(vector<vector<int>>& nums, int k) {
+        sort(nums.begin(), nums.end());
         n = nums.size();
-        sort(nums.begin(),nums.end());
-        vector<p>dp(k+1, p(n+1, -1));
-        return solve(nums, k,0, -1,dp);
+        dp.resize(n+1, vector<int>(k+1, -1));
+        return solve(nums, k, 0);
     }
 };
