022f89cc48189d2e1a3c4f39b89e1dbbe93b7a3e
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-16 20:42:15 +0530

    Time: 2280 ms (24.07%), Space: 59.8 MB (12.61%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int n ;
-    
-//     bool isPalindrome(string S){
-//         int i=0;
-//         int j=S.length()-1;
-        
-//         while(j>i){
-//             if(S[i]==S[j]){
-//                 i++;
-//                 j--;
-//             }
-//             else{
-//                 return false;
-//             }
-//         }
-        
-//         return true;
-//     }
-    vector<vector<int>>dp;
-    
-    bool isPalindrome(int i, int j, string &s) {
+    int n;
+    vector<vector<int>> dp;
+    // bool isPalindrome(string s,int i,int j){
+    //     while(i<=j){
+    //         if(s[i]!=s[j]){
+    //             return false;
+    //         }
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+     bool isPalindrome(string &s, int i, int j) {
         while (i < j) {
             if (s[i] != s[j]) return false;
             i++;
@@ -28,25 +20,34 @@ public:
         }
         return true;
     }
-
-    int solve(string &s, int st, int end){
-        if (st >=end ) return 0;
-        if(dp[st][end]!=-1)return dp[st][end];
-        int ans = INT_MAX;
-        for(int k = st; k<end; k++){
-            // string temp = s.substr(st,k);
-            if(isPalindrome(st,k,s)){
-                int cnt = 1 + solve(s, k+1, end);
-                ans = min(ans, cnt);
+    int solve(string &s,int i,int j){
+        if(i>=j){
+            return 0;
+        }
+        // if(isPalindrome(s,i,j)){
+        //     return 0;
+        // }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=INT_MAX;
+        for(int k=i;k<j;k++){
+            if(isPalindrome(s,i,k)){
+                int a=1+solve(s,k+1,j);
+                ans=min(ans,a);
             }
         }
-        return dp[st][end] = ans;
+        return dp[i][j]=ans;
     }
-   
-    
     int minCut(string s) {
-        n= s.size();
+        n=s.size();
+        if(n==0){
+            return 0;
+        }
+        if(isPalindrome(s,0,s.size()-1)){
+            return 0;
+        }
         dp.resize(n+1,vector<int>(n+1,-1));
-        return solve(s,0,n) -1  ;
+        return solve(s,0,n)-1;
     }
 };
