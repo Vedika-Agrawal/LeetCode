bf8161dcbd70c319ccda849fc8d790496edf1cbb
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-20 12:28:34 +0530

    Time: 139 ms (5.72%), Space: 20.4 MB (6.31%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& v, int n) {
-        if(n>v.size())return false;
-        
-        int i=0;
-        while(i<v.size()){
-            int p = i-1, f = i+1; 
-            if(v[i]==0){
-                if(p>=0 && f<v.size()){
-                    if(v[p]==0 && v[f]==0){
-                        n--;
-                        i++;
-                    }
-                }
-                if(p==-1 && f==1 && v.size()==1){
-                    return true;
-                }
-                    if(p==-1 && f<v.size() && v[f]==0){
-                        n--;
-                        i++;
-                    }
-                    if( f==v.size() && p>=0 && v[p]==0 ){
-                        n--;
-                        i++;
-                    }
-                
-                    
-                
+        int sz = v.size();
+        for(int i=0;i<v.size();i++){
+            cout<<i<<endl;
+            if(n==0)break;
+            if(v[i]==0 && i==0 && i==sz-1){
+                cout<<i<<endl;
+                i++;
+                n--;
             }
-            i++;
+            else if(v[i]==0 && i==0 && i+1<sz && v[i+1]==0){
+                cout<<i<<endl;
+                i++;
+                n--;
+            }
+            else if(v[i]==0 && i-1 >= 0 && i+1<sz && v[i+1]==0 && v[i-1]==0){
+                cout<<i<<endl;
+                i++;
+                n--;
+            }
+            else if(v[i]==0 && i-1 >= 0 && i+1==sz  && v[i-1]==0){
+                cout<<i<<endl;
+                i++;
+                n--;
+            }
         }
-        
-        return n<=0 ? true : false;
+        if(n==0)return true;
+        return false;
     }
 };
