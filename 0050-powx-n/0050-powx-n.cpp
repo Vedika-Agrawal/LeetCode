f941f245325b99454248e01c5aa44a4f8afe5ddb
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-24 15:13:24 +0530

    Time: 4 ms (40.18%), Space: 5.9 MB (62.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        
-        if(n==0){
-            return 1;
-        }
-        if (n < 0) { 
-            n = abs(n);
-            x = 1/x;
+    double solve(double x, long n){
+        if(n==0)return 1;
+        if(n<0){
+            return solve(1/x, -1*n);
         }
         if(n%2==0){
-            return myPow(x*x,n/2);
-        
-        }else{
-            return x*myPow(x*x,n/2);
+            return solve(x*x, n/2);
         }
+        else{
+            return solve(x*x,n/2)*x;
+        }
+    }
+    double myPow(double x, int n) {
+        return solve(x,(long)n);
     }
 };
