c180467ab0a839d1d9f0c829309055a2873d0e8d
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-27 10:20:49 +0530

    Time: 99 ms (100.00%), Space: 55.7 MB (98.56%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     #define  ll long long 
     
-    
-   bool isPossible(vector<int>& batteries, long long time, int computers){
+  bool isPossible(vector<int>& batteries, long long time, int computers){
         long long totTime = time*computers;
         
         for(long long bTime : batteries)
@@ -11,29 +10,25 @@ public:
         
         return (totTime <= 0);//if entire charge for all computers is drained, then we used all batteries, so it's a possible solution
     }
-    long long maxRunTime(int n, vector<int>& batteries) {
-        long long low = 0, high = 0;
-        int si = batteries.size();
+    
+    
+
+
+    ll maxRunTime(int n, vector<int>& bat) {
+        // sort(bat.rbegin(),bat.rend());
         
-        for(int i = 0; i < si; i++){
-            high += batteries[i];
-        }
-        
-        long long ans = 0;
-        while(low <= high){
-            
-            long long mid = low + (high-low)/2;
-            
-            if(isPossible(batteries, mid, n)){//asking for max minutes => increase the time to check if that's possible
-                ans = mid;
-                low = mid+1;
+        ll h  = accumulate(bat.begin(),bat.end(), (ll)0);
+        ll l = 0;
+        while(l<=h){
+            ll mid = l + (h-l)/2;
+            cout<<l<<" "<<mid<<" "<<h<<endl;
+            if(isPossible(bat, mid, n)){
+                l = mid+1;
             }
             else{
-                 high = mid-1;//if this particular time isn't possible, reduce the range to mid-1
+                h = mid-1;
             }
         }
-        
-        return ans;
+        return l-1;
     }
-	
 };
