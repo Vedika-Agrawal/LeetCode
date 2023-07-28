8277163c5dea857cb364e9b01cc7f5efb6cb3c42
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-28 22:46:34 +0530

    Time: 4 ms (63.90%), Space: 7.5 MB (43.52%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     
-       
+       vector<vector<int>>dp;
     int solve(vector<int>&nums, int i, int j, int flag){
         if(i>j) return 0;
         if(i==j)return nums[i];
-       
+       if(dp[i][j]!=-1)return dp[i][j];
           int var = 0 ;
         
         if(flag==0){
@@ -16,10 +16,12 @@ public:
         
             var  += min(- nums[i] + solve(nums, i+1, j, 0) ,  -nums[j] + solve(nums, i, j-1,0));
         }
-        return var ;
+        return dp[i][j] = var ;
         
     }
     bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        dp.resize(n+1, vector<int>(n+1, -1));
         return solve(nums, 0, nums.size()-1, 0)>=0;
     }
 };
