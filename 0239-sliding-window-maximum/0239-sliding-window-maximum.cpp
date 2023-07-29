8c4622055be047dec3d48524f4c22b171c7ceb6c
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-07-30 00:16:12 +0530

    Time: 207 ms (99.89%), Space: 134.7 MB (66.37%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -15,3 +15,25 @@ public:
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& v, int k) {
+//         vector<int>ans;
+//         priority_queue<pair<int,int>>q;
+//         for(int i=0;i<k;i++)q.push({v[i], i});
+//         ans.push_back(q.top().first); // 3 3 
+        
+//         for(int i=k;i<v.size();i++){
+//             q.push({v[i], i}); // add curr ele to the q; // -1=0 3=1 -1=2 -3=3 5=4
+            
+//             while(q.top().second<=i-k) q.pop();
+               
+//             ans.push_back(q.top().first);
+            
+            
+            
+//         }
+//         return ans;
+//     }
+// };
