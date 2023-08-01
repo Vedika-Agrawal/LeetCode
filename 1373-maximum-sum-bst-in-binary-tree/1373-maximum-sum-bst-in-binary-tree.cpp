c1b0d2e3c54d71149b093b4b0b3bb5de840f8d3c
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-08-02 00:27:40 +0530

    Time: 151 ms (97.00%), Space: 111.7 MB (95.93%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -7,38 +7,39 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- 
  * };
  */
 class Solution {
 public:
-    int res  = 0;
-    class bst{
-       public :
-        bool isBst;
-        int mn , mx, sum;
+    struct check{
+        bool isBst ;
+        int mn ;
+        int mx;
+        int sum ;
     };
     
-    bst solve(TreeNode* root){
-        
-        if(!root) return {true, INT_MAX, INT_MIN,0};
-        
-        auto l = solve(root->left);
-        auto r = solve(root->right);
-        
-        if(l.isBst && r.isBst && l.mx < root->val && root->val < r.mn){
-            int total = l.sum + r.sum + root->val;
-            res = max(res, total);
-            
-            return {true, min(l.mn, root->val), max(r.mx , root->val), total};
+    int ans  = 0 ;
+    
+    check solve(TreeNode* root){
+        if(!root){
+            return {true, INT_MAX, INT_MIN, 0};
         }
-        else{
-            return { false, 0, 0, 0 };
+        
+        check l = solve(root->left);
+        check r = solve(root->right);
+        if(l.isBst && r.isBst && l.mx < root->val && r.mn > root->val ){
+            int total = l.sum + r.sum+root->val;
+            ans = max(ans,total);
+            return {true, min(l.mn,root->val), max(root->val,r.mx), total };
         }
+        return {false, 0,0,0};
+        
         
     }
+    
     int maxSumBST(TreeNode* root) {
         solve(root);
-        return res;
+        
+        return ans;
     }
 };
