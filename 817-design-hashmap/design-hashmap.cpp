9a6835b9ea4135838d0181c8530f3bde10c58049
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2023-10-04 12:05:10 +0530

    Time: 563 ms (5.06%) | Memory: 338.7 MB (5.33%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.cpp
@@ -0,0 +1,30 @@
+class MyHashMap {
+public:
+    vector<int>mp;
+    int size = 0;
+    MyHashMap() {
+        size = 10e6;
+        mp.resize(size);
+        fill(mp.begin(),mp.end(),-1);
+    }
+    
+    void put(int key, int value) {
+        mp[key]=value;
+    }
+    
+    int get(int key) {
+        return mp[key];
+    }
+    
+    void remove(int key) {
+        mp[key]=-1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
