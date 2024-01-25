967487c7bf56c919205134ced09c87dca91798d5
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-01-26 00:06:08 +0530

    Time: 12 ms (95.31%) | Memory: 13.4 MB (6.68%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -1,29 +1,62 @@
+// class Solution {
+
+// public:
+//     int solve(vector<int>&jd, int d, int currIdx, int mx){
+//         if(currIdx==jd.size()){
+//             if(d==0){
+//                 return mx;
+//             }
+//             return INT_MAX;
+//         }
+//         int take =INT_MAX;
+//         int notTake = INT_MAX;
+
+
+//         // noTake currIdx diff grp ;
+//         notTake =  mx + solve(jd, d-1, currIdx+1, jd[currIdx]);
+        
+//         //  take = = same grp continue;
+//         take = solve(jd, d, currIdx+1, max(mx, jd[currIdx]));
+//         return min(notTake, take);
+//     }
+//     int minDifficulty(vector<int>& jd, int d) {
+//         int n = jd.size();
+//         if(d>n)return -1;
+//         return solve(jd,d,0, 0);
+//     }
+// };
+
 class Solution {
 public:
-    int solve(vector<int>&jd, int d, int idx, vector<vector<int>>&dp){
-        if(d==1){
-            int mx = jd[idx];
-            for(int i= idx; i<jd.size(); i++){
-                mx = max(mx, jd[i]);
-            }
-            return mx;
+    int n;
+    int dp[301][11][302];
+    int solve(vector<int>& job,int idx,int mx,int d){
+        if(d==0)return 1e9;
+        if(idx>=n) return d==1 ? job[mx] : 1e9;
+        
+        if(dp[idx][d][mx]!=-1){
+            return dp[idx][d][mx];
         }
-        if(dp[idx][d]!=-1)return dp[idx][d];
-        int ans = 1e9;
-        int mx = INT_MIN;
-        for(int i = idx; i<jd.size()-d+1; i++){
-            mx = max(mx, jd[i]);
-            int temp = mx + solve(jd, d-1, i+1,dp);
-            ans = min(ans, temp);
-        }
-        return dp[idx][d]=  ans;
+
+        
+        // 1. Either carry the current task in previous day
+        int temp_idx = job[idx]> job[mx] ? idx : mx;
+         int b=solve(job,idx+1,temp_idx,d);
+
+
+        // 2. Finish the day and start current task in new day
+        int a= job[mx] + solve(job,idx+1,idx,d-1);
+
+       
+
+        return dp[idx][d][mx] = min(a,b);
     }
-    int minDifficulty(vector<int>& jobDifficulty, int d) {
-        int n = jobDifficulty.size();
-        vector<vector<int>>dp(n+1, vector<int>(d+1, -1));
-        if(d>n)return -1;
-        return solve(jobDifficulty, d, 0, dp);
+    int minDifficulty(vector<int>& job, int d) {
+        n=job.size();
+        memset(dp,-1,sizeof(dp));
+        if(d>n){
+            return -1;
+        }
+        return solve(job,1,0,d);
     }
 };
-
- 
