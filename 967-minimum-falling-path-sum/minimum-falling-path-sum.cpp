91d82b0f17930673ab1bc57dda8ef378d621dbb6
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-01-28 21:48:19 +0530

    Time: 13 ms (39.82%) | Memory: 13.7 MB (5.40%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    #define ll long long
-    int n, m;
-    vector<vector<int>>dp;
-    ll solve(int r, int c, vector<vector<int>>&mat){
-        if(r<0 || c<0 || r>=n || c>=m)return 1e9;
-        if(r==n-1)return mat[r][c];
-        if(dp[r][c]!=1e9+1)return dp[r][c];
-        ll a = (ll)mat[r][c] + solve(r+1,c-1,mat);
-        ll b = (ll)mat[r][c] + solve(r+1,c,mat);
-        ll d = (ll) mat[r][c] + solve(r+1,c+1,mat);
-        return dp[r][c] = min({a,b,d});
-
-    }
+#define ll long long
+    
 
     int minFallingPathSum(vector<vector<int>>& mat) {
-        int ans = INT_MAX;
-         n = mat.size();
-         m = mat[0].size();
-         dp.resize(n,vector<int>(m,1e9+1));
-        for(int i=0;i<m;i++){
-            int temp_ans = solve(0,i, mat);
-            ans = min(ans, temp_ans);
+    
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 0; i < m; i++) {
+            dp[n - 1][i] = mat[n - 1][i];
+        }
+
+        for (int r = n - 2; r >= 0; r--) {
+            for (int c = 0; c < n; c++) {
+               ll b = 1ll * mat[r][c] + dp[r + 1][c];
+                ll a = 1ll * mat[r][c] + (c + 1 < m ? dp[r + 1][c + 1] : 1e5);
+                ll d = 1ll * mat[r][c] + (c - 1 >= 0 ? dp[r + 1][c - 1] : 1e5);
+                dp[r][c] = min({a, b, d});
+            }
+        }
+        int ans = 1e9;
+        for(int c =0;c<m;c++){
+            ans = min(ans, dp[0][c]);
         }
         return ans;
     }
