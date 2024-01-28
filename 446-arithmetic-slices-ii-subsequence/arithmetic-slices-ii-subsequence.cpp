094423b9c8a3e6cf1c1a670763010c10bda60f03
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-01-28 17:04:36 +0530

    Time: 424 ms (68.60%) | Memory: 158.3 MB (21.71%) - LeetSync

--- 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.cpp
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int> &nums) {
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if (nums.size() < 3) {
+            return 0;
+        }
+
         int n = nums.size();
-        int ans = 0;
-        vector<unordered_map<long long, int>> dp(n); // dp[i][d]
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                long long diff = (long long) nums[i] - nums[j];
-                int cnt = dp[j].count(diff) ? dp[j][diff] : 0;
-                dp[i][diff] += cnt + 1;
-                ans += cnt;
+        vector<unordered_map<long long, int>> dp(n);
+
+        int result = 0;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long long diff = 1ll * nums[i] - nums[j];
+                if (dp[j].find(diff) != dp[j].end()) {
+                    dp[i][diff] += dp[j][diff] + 1;
+                    result += dp[j][diff];
+                } else {
+                    dp[i][diff] += 1;
+                }
             }
         }
-        return ans;
+
+        return result;
     }
 };
