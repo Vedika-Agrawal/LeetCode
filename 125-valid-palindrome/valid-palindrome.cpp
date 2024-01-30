1814beb25afd74af114d97bcbe60b7d2efd900b0
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-01-30 14:59:10 +0530

    Time: 4 ms (80.54%) | Memory: 8.6 MB (17.21%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    bool isPalindrome(string& s) {
-        int i = 0, j = 0, n = s.size();
-        while (i < n) {
-            while (i < n && !isalnum(s[i]))
-                i++;
-            if (i < n) {
-                char temp = tolower(s[i]);
-                s[j] = temp;
-                j++;
-                i++;
-            }
-        }
-        return checkPalindrome(s, 0, j);
-    }
-
-    bool checkPalindrome(string S, int st, int end) {
+  bool checkPalindrome(string S, int st, int end) {
         for (int i = 0; i < end / 2; i++) {
             if (S[i] != S[end - i - 1]) {
                 return false;
             }
         }
         return true;
+    }
+
+    bool isVaildChar(char ch) {
+        if (ch >= 'a' && ch <= 'z')
+            return true;
+        else if (ch >= 'A' && ch <= 'Z')
+            return true;
+        else if (ch >= '0' && ch <= '9')
+            return true;
+        return false;
+    }
+    bool isPalindrome(string&s) {
+        int i = 0, j = 0, n = s.size();
+        while (i < n) {
+            while (i < n && isVaildChar(s[i]) == false)
+                i++;
+            if (i <n) {
+                char temp = tolower(s[i]);
+                s[j] = temp;
+                j++;
+                i++;
+            } 
+        }
+        return checkPalindrome(s,0,j);
+        return false;
     }
 };
