ddf9e415db9c79283785554f4b5eea41403e90d1
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-02-13 14:10:59 +0530

    Time: 133 ms (74.69%) | Memory: 92.6 MB (6.12%) - LeetSync

--- 1831-ways-to-split-array-into-three-subarrays/ways-to-split-array-into-three-subarrays.cpp
+++ 1831-ways-to-split-array-into-three-subarrays/ways-to-split-array-into-three-subarrays.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int mod = 1e9+7;
+    vector<int> prefixSum;
     int totalSum;
     int n;
-    int solve( vector<int>&prefixSum) {
+    int solve() {
         int cnt = 0;
         for (int left = 0; left < n-2; left++) {
             int leftSum = prefixSum[left];
 
             int midEnd = (totalSum - leftSum)/2;
-            int low = lower_bound(prefixSum.begin()+left+1, prefixSum.end(), leftSum *2)-prefixSum.begin();
-            int up = upper_bound(prefixSum.begin()+ left + 1, prefixSum.begin()+n -1, midEnd+leftSum)-prefixSum.begin();
-            if(up - low >0) cnt = (cnt + up - low)% mod;
+            int low = lower_bound(prefixSum.begin()+left+1, prefixSum.end(), leftSum*2)-prefixSum.begin();
+            int up = upper_bound(prefixSum.begin() + low, prefixSum.begin()+n-1, midEnd+leftSum)-prefixSum.begin();
+            cnt = (cnt + up - low)% mod;
         }
         return cnt%mod;;
     }
     int waysToSplit(vector<int>& nums) {
         n = nums.size();
-        for(int i =1;i<n;i++) nums[i] += nums[i-1];
-        totalSum += nums[n-1];
-        return solve(nums);
+        int sum = 0;
+        for (auto it : nums) {
+            sum += it;
+            prefixSum.push_back(sum);
+        }
+        totalSum += prefixSum[n-1];
+        return solve();
     }
 };
-
 
