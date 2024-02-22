f6fe42d50b5bd81d063cc20bfe445fe55d7eedd0
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-02-22 10:31:50 +0530

    Time: 149 ms (44.69%) | Memory: 72.4 MB (21.11%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,18 +1,15 @@
+
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>v(n+1,0);
-        for(auto it: trust){
-            v[it[0]]++;
-            v[it[1]]--;
-        }
-        
-        int neg = 0;
-        for(int i = 1; i<=n;i++){
-            if(-1 *v[i]== n-1)return i;
-        }
-
-         return -1;
-        
+    int findJudge(int N, vector<vector<int>>& trust) {
+		vector<int> in(N + 1), out(N + 1);
+		for (auto a : trust) {
+			++out[a[0]];
+			++in[a[1]];
+		}
+		for (int i = 1; i <= N; ++i) {
+			if (in[i] == N - 1 && out[i] == 0) return i;
+		}
+		return -1;
     }
 };
