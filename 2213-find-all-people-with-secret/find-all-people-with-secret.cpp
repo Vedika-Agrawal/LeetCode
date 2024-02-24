c14944cde940691cabb8620d97b78bf6f20c195d
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-02-24 14:34:03 +0530

    Time: 844 ms (30.33%) | Memory: 207.5 MB (31.97%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -11,15 +11,15 @@ public:
         }
     }
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        // mp[0].push_back({firstPerson, 0});
+        mp[0].push_back({firstPerson, 0});
         for(auto it: meetings){
             mp[it[0]].push_back({it[1], it[2]});
             mp[it[1]].push_back({it[0], it[2]});
         }
         vis.resize(n, 1e9);
         vis[0] = 0 ;
-        vis[firstPerson] = 0;
-        dfs(firstPerson, 0);
+    
+      
         dfs(0,0);
         vector<int> ans;
         for (int i = 0; i < n; ++i) {
