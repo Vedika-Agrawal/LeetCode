6bed399a506219141d3aa71732732acc186b309a
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-02-24 14:32:22 +0530

    Time: 807 ms (31.15%) | Memory: 207.6 MB (31.97%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    void dfs(int i, int time, unordered_map<int, vector<pair<int,int>>>& graph, vector<int>& visitedAt) {
-        for (auto neigh : graph[i]) {
-            if (time <= neigh.second && neigh.second < visitedAt[neigh.first]) {
-                visitedAt[neigh.first] = neigh.second;
-                dfs(neigh.first, neigh.second, graph, visitedAt);
+    unordered_map<int, vector<pair<int,int>>> mp;
+    vector<int> vis;
+    
+    void dfs(int node, int time) {
+        for (auto it : mp[node]) {
+            if (it.second >= time && vis[it.first] > it.second) {
+                vis[it.first] = it.second;
+                dfs(it.first, it.second);
             }
         }
     }
-
+    
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        unordered_map<int, vector<pair<int,int>>> graph;
-        for (int i = 0; i < n; ++i)
-            graph[i] = vector<pair<int,int>>();
-
-        for (auto meet : meetings) {
-            graph[meet[0]].push_back({meet[1], meet[2]});
-            graph[meet[1]].push_back({meet[0], meet[2]});
+        for (auto it : meetings) {
+            mp[it[0]].push_back({it[1], it[2]});
+            mp[it[1]].push_back({it[0], it[2]});
         }
-
+        
+        vis.resize(n, 1e9);
+        vis[0] = 0;
+        vis[firstPerson] = 0;
+        
+        dfs(0, 0);
+        dfs(firstPerson, 0);
+        
         vector<int> ans;
-        vector<int> visitedAt(n, INT_MAX);
-        visitedAt[0] = 0;
-        visitedAt[firstPerson] = 0;
-        dfs(firstPerson, 0, graph, visitedAt);
-        dfs(0, 0, graph, visitedAt);
         for (int i = 0; i < n; ++i) {
-            if (visitedAt[i] != INT_MAX)
+            if (vis[i] != 1e9)
                 ans.push_back(i);
         }
         return ans;
