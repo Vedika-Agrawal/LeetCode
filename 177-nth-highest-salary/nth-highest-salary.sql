506e632115f5abf17b9d86b198e7fda5b80b4cf7
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-03-24 12:18:49 +0530

    Time: 814 ms (64.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+
+BEGIN
+set N = N - 1;
+  RETURN (
+    SELECT DISTINCT salary FROM employee ORDER BY salary DESC LIMIT 1 OFFSET N
+  );
+END
