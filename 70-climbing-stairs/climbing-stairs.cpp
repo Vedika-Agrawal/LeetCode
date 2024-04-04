b816615cee339c1ffb73f6035c1c5dccd7fc4bf2
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-04-04 13:25:37 +0530

    Time: 3 ms (28.21%) | Memory: 7.4 MB (31.14%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<int>dp;
-    int solve(int n, int i){
-        if(i>n)return 0;
-        if(i==n)return 1;
-        if(dp[i]!=-1)return dp[i];
-        int a = solve(n, i+1);
-        int b = solve(n, i+2);
-        return dp[i] =a+b;
+    int solve(int n){
+        if(n==0)return 1;
+        if(n<0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n] = solve(n-1) + solve(n-2);
     }
+    
     int climbStairs(int n) {
-        dp.resize(n+1, -1);
-        return solve(n,0);
+      dp.resize(n+1,-1);
+      return solve(n);
     }
 };
