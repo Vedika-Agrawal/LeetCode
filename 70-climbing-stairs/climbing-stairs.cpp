10c92051fdea35e1ef48aa8d390dc57a5608a4f7
Vedika Agrawal <78415234+Vedika-Agrawal@users.noreply.github.com>
2024-04-04 13:28:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (11.84%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,7 +9,12 @@ public:
     }
     
     int climbStairs(int n) {
-      dp.resize(n+1,-1);
-      return solve(n);
+      dp.resize(n+1,0);
+      dp[0] = 1;
+      dp[1] = 1;
+      for(int i = 2; i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2]; 
+      }
+      return dp[n];
     }
 };
